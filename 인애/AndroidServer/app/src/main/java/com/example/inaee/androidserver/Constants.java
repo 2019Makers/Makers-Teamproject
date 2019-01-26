85ce2b41f115bb9706a62f65a47af6e37fbcb24e
Hwang Inae <ghkddlsdo110@gmail.com>
2019-01-26 16:00:16 +0900

    AndroidServer
    android 클라이언트

---
+++ 인애/AndroidServer/app/src/main/java/com/example/inaee/androidserver/Constants.java
@@ -0,0 +1,14 @@
+package com.example.inaee.androidserver;
+
+public class Constants {
+
+    public static final boolean PLAY = true;
+    public static final boolean NEXT =true;
+    public static final boolean PREVIOUS = true;
+    public static final int SERVER_PORT = 8998;
+    public static final boolean MOUSE_LEFT_CLICK = true;
+
+
+    public static final String SERVER_IP = 192.168.0.22;
+
+}
